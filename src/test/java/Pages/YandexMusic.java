d5c565a12aec7bec0d9f27992545aabf26ed4077
Oleksandr_Mosin@epam.com <Oleksandr_Mosin@epam.com>
2017-02-09 19:22:04 +0200

    commit

--- src/test/java/Pages/YandexMusic.java
+++ src/test/java/Pages/YandexMusic.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,7 +31,8 @@ public class YandexMusic {
     static By searchPageTitle = By.className("srg");
     static By btnSearch = By.xpath("//button[@type =\"submit\"]");
     By songsList = By.xpath("//*[@class = \"track track_type_full track_selectable\"]");
-    By sidebarPlay = By.xpath("//*[@id=\"nb-1\"]//*[@class=\"sidebar-track__controls\"]//*[@class=\"icon icon_pp icon_size_L\"]");
+    By sidebarPlay = By
+            .xpath("//*[@id=\"nb-1\"]//*[@class=\"sidebar-track__controls\"]//*[@class=\"icon icon_pp icon_size_L\"]");
     By trackHeader = By.xpath("//*[@class = \"link link_arrow\"]");
 
     public void openHomePage() {
@@ -42,12 +44,21 @@ public class YandexMusic {
         driver.findElement(btnSearch).click();
     }
 
-    public void playSong(){
-        WebElement actual = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(trackHeader));
-        List<WebElement> list =  driver.findElements(songsList);
+    public void playSong() {
+        scrollWithOffset(driver.findElement(searchField), 0, 1000);
+        WebElement actual = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(trackHeader));
+        List<WebElement> list = driver.findElements(songsList);
         list.get(0).click();
         WebElement play = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(sidebarPlay));
         play.click();
+    }
+
+    public void scrollWithOffset(WebElement webElement, int x, int y) {
+        String code = "window.scroll(" + (webElement.getLocation().x + x) + "," + (webElement.getLocation().y + y)
+                + ");";
+        ((JavascriptExecutor) driver).executeScript(code, webElement, x, y);
+
     }
 
     public void close() {
